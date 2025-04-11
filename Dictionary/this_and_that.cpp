b1042aee59f8d7e9458618e1d1983df56fdad93d
jwpp21 <131251204+jwpp21@users.noreply.github.com>
2025-04-11 16:28:00 +0900

    Update this_and_that.cpp

--- Dictionary/this_and_that.cpp
+++ Dictionary/this_and_that.cpp
@@ -7,3 +7,11 @@ while(sum!=0) {
 
 to_string() // int to string
 stoi() // string to int
+
+// 2차원 배열 초기화
+int num[10][10] = {0, };
+
+// 2차원 배열 입력 for문 이용
+for(int i=0;i<10;i++){
+	cin >> num[i]; // num배열 i번째 줄 전부에 입력 ex) 1303입력시 num[0][0] = 1, num[0][1] = 3 
+}
