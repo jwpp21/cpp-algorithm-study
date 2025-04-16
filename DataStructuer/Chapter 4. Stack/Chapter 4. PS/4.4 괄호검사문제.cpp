03128e0af61f976a94f6017775f4224a446ccd66
jwpp21 <131251204+jwpp21@users.noreply.github.com>
2025-04-16 16:14:46 +0900

    Update 4.4 괄호검사문제.cpp

--- DataStructuer/Chapter 4. Stack/Chapter 4. PS/4.4 괄호검사문제.cpp
+++ DataStructuer/Chapter 4. Stack/Chapter 4. PS/4.4 괄호검사문제.cpp
@@ -1,1 +1,30 @@
 
+int checkMatching(const char* in) {
+	StackType* s;
+	char ch, open_ch;
+	int i, n = strlen(in);
+	init_stack(s);
+
+	for (i = 0;i < n;i++) {
+		ch = in[i];
+		switch (ch)
+		{
+		case '(': case '[': case '{':
+			push(s, ch);
+			break;
+		case ')': case ']': case '}':
+			if (isEmpty(s)) return 0;
+			else {
+				open_ch = pop(s);
+				if ((open_ch == '(' && ch != ')') ||
+					(open_ch == '[' && ch != ']') ||
+					(open_ch == '{' && ch != '}')) {
+					return 0;
+				}
+				break;
+			}
+		}
+	}
+	if (!isEmpty(s)) return 0;
+	return 1;
+}
