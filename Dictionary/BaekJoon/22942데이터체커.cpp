df3992806b4f48c919590c6dd31c8cbd840136ab
jwpp21 <131251204+jwpp21@users.noreply.github.com>
2025-04-28 18:27:06 +0900

    Update 22942데이터체커.cpp

--- Dictionary/BaekJoon/22942데이터체커.cpp
+++ Dictionary/BaekJoon/22942데이터체커.cpp
@@ -50,3 +50,59 @@ int main() {
 
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+
+    int n;
+    cin >> n;
+    vector<pair<int, int>> circles(n);
+
+    while (n--)
+    {
+        int x, r;
+        cin >> x >> r;
+
+        circles[n] = make_pair(x - r, x + r);
+    }
+
+    sort(circles.begin(), circles.end(), [](pair<int, int> left, pair<int, int> right) {
+        return left.first != right.first ? left.first < right.first : left.second < right.second;
+        });
+
+    bool is_valid = true;
+    vector<int> end_stack;
+
+    for (auto circle : circles)
+    {
+        while (!end_stack.empty() && end_stack.back() < circle.first)
+        {
+            end_stack.pop_back();
+        }
+
+        if (!end_stack.empty() && circle.first <= end_stack.back() && end_stack.back() <= circle.second)
+        {
+            is_valid = false;
+            break;
+        }
+
+        end_stack.push_back(circle.second);
+    }
+
+    if (is_valid) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
